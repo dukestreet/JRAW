600108bb9db93d7da7ac563838c220c31c1c0ed7
dukestreet <anonymous@gmail.com>
2022-02-18 02:26:31 -0500

    Migrate WikiPage to kotlin

---
+++ lib/src/main/java/net/dean/jraw/models/WikiPage.kt
@@ -0,0 +1,35 @@
+package net.dean.jraw.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import net.dean.jraw.databind.Enveloped
+import net.dean.jraw.databind.RedditModel
+import net.dean.jraw.databind.UnixTime
+import java.io.Serializable
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+@RedditModel
+@JsonClass(generateAdapter = true)
+data class WikiPage(
+    @Json(name = "may_revise") val mayRevise: Boolean,
+
+    /** The last time this page was edited, or null if never.  */
+    @Json(name = "revision_date")
+    @UnixTime(precision = TimeUnit.SECONDS)
+    val revisionDate: Date?,
+
+    /** The person that last revised this page, or null if never revised.  */
+    @Enveloped
+    @Json(name = "revision_by")
+    val revionBy: Account?,
+
+    /** The Markdown-formatted body of the page  */
+    @Json(name = "content_md") val content: String?,
+) : Serializable {
+
+    /** True if there is an authenticated user and that user has the privileges to edit this wiki page  */
+    fun mayRevise(): Boolean {
+        return mayRevise
+    }
+}
