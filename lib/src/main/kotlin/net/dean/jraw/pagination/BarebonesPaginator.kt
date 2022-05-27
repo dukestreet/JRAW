d5855e2d786aa8bf709c586fe69dffbcaa14d3aa
dukestreet <anonymous@gmail.com>
2022-05-27 01:35:20 -0400

    Deprecate BarebonesPaginator#customAnchor() in favor of after()

--- lib/src/main/kotlin/net/dean/jraw/pagination/BarebonesPaginator.kt
+++ lib/src/main/kotlin/net/dean/jraw/pagination/BarebonesPaginator.kt
@@ -12,14 +12,14 @@ open class BarebonesPaginator<T : UniquelyIdentifiable> private constructor(
     reddit: RedditClient,
     baseUrl: String,
     limit: Int,
-    private val customAnchorFullName: String?,
+    private val afterFullName: String?,
     clazz: Class<T>
 ) : Paginator<T>(reddit, baseUrl, limit, clazz) {
 
     override fun createNextRequest(): HttpRequest.Builder {
         val args = mutableMapOf("limit" to limit.toString())
 
-        val anchor = customAnchorFullName ?: current?.nextName
+        val anchor = afterFullName ?: current?.nextName
         if (anchor != null)
             args["after"] = anchor
 
@@ -34,16 +34,20 @@ open class BarebonesPaginator<T : UniquelyIdentifiable> private constructor(
 
         /** How many items to request at once */
         private var limit: Int = Paginator.DEFAULT_LIMIT
-        private var customAnchorFullName: String? = null
+        private var afterFullName: String? = null
 
         /** How many items to request at once */
         fun limit(limit: Int): Builder<T> { this.limit = limit; return this }
 
         /** Sets a custom pagination anchor */
-        fun customAnchor(anchorFullName: String): Builder<T> { this.customAnchorFullName = anchorFullName; return this }
+        @Deprecated("Use after() instead", ReplaceWith("after(anchorFullName)"))
+        fun customAnchor(anchorFullName: String): Builder<T> = after(anchorFullName)
+
+        /** Sets a custom pagination anchor */
+        fun after(afterFullName: String?): Builder<T> { this.afterFullName = afterFullName; return this }
 
         override fun build(): BarebonesPaginator<T> =
-            BarebonesPaginator(reddit, baseUrl, limit, customAnchorFullName, clazz)
+            BarebonesPaginator(reddit, baseUrl, limit, afterFullName, clazz)
 
         /** */
         companion object {
