31bf62394648fbe844f31fe3c82229eec2316787
dukestreet <anonymous@gmail.com>
2022-05-27 01:34:23 -0400

    Add video related fields to MediaMetadata

--- lib/src/main/kotlin/net/dean/jraw/models/MediaMetadata.kt
+++ lib/src/main/kotlin/net/dean/jraw/models/MediaMetadata.kt
@@ -18,5 +18,8 @@ data class MediaMetadataItem(
     @Json(name = "e") val kind: String?,
     @Json(name = "m") val mime: String?,
     @Json(name = "p") val previews: List<MediaMetadataPreview>?,
-    @Json(name = "s") val full: MediaMetadataPreview?
+    @Json(name = "s") val full: MediaMetadataPreview?,
+    @Json(name = "dashUrl") val videoDashUrl: String?,
+    @Json(name = "x") val x: Int?,  // I've noticed that x,y are only present for videos in galleries.
+    @Json(name = "y") val y: Int?,
 )
